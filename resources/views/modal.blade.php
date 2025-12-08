dd296d7725bb499efe6bd3ef4a5901c0ffdfc907
Andi Siahaan <andisiahaan670@gmail.com>
2025-12-09 03:44:30 +0700

    fix: remove dark mode background class from modal backdrop and adjust modal width class

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -28,7 +28,7 @@
                 x-transition:leave-start="opacity-100"
                 x-transition:leave-end="opacity-0"
                 class="fixed inset-0 transition-all transform">
-                <div class="absolute inset-0 bg-gray-500 dark:bg-gray-900 opacity-75"></div>
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
 
             {{-- Centering trick --}}
@@ -44,7 +44,7 @@
                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                 x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 x-bind:class="modalWidth"
-                class="relative inline-block w-full align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full"
+                class="relative inline-block w-full align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full"
                 id="modal-container"
                 x-trap.noscroll.inert="show && showActiveComponent"
                 aria-modal="true">
